291d21ca173f4acb3569e082e857407d765fce37
Nam Wook Kim <namwkim85@gmail.com>
2016-01-19 22:43:32 -0500

    update period changed

--- site/routes/socket.js
+++ site/routes/socket.js
@@ -10,20 +10,33 @@ module.exports = function(io, db){
     months += d2.getMonth();
     return months <= 0 ? 0 : months;
   }
+  function dayDiff(d1, d2) {
+    return Math.round((d2-d1)/(1000*60*60*24));
+  }
 
 	io.on('connection', function (socket) {
     var today = new Date();
 		connectedUsers++;
     io.emit("update-users", connectedUsers);
     // retrieve the most recent record
-    db.collection('canvas').find().sort({$natural:1}).limit(1)
+    db.collection('canvas').find().sort({$natural:-1}).limit(1)
     .toArray(function(err, results){
       var canvas = null;
       if (results.length!=0){
         var date = dateFromObjectId(results[0]._id.toString());
-        var diff = monthDiff(today, date);
+        var diff = dayDiff(date, today);
+        if (diff>7){
+          db.collection('canvas').insert({project:null}, function(err, result) {
+            if (err) {
+                return console.log(new Date(), 'insert error', err);
+            }
+          });
+        }else{
+          canvas = results[0].project;
+        }
+        // console.log(results[0]._id)
+        // console.log("canvas"  + canvas)
 
-        canvas = diff>0? null:results[0].project;
       }else{
         db.collection('canvas').insert({project:null}, function(err, result) {
             if (err) {
@@ -38,9 +51,10 @@ module.exports = function(io, db){
   	socket.on('update', function (canvas) {
   		console.log('broadcast new canvas');
       //save into the most recent record
-      db.collection('canvas').find().sort({$natural:1}).limit(1)
+      db.collection('canvas').find().sort({$natural:-1}).limit(1)
       .toArray(function(err, results){
         if (results.length!=0){
+          // console.log(results[0]._id)
           db.collection('canvas').update({_id:results[0]._id},
             {
               '$set': { project: canvas }
