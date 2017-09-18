82cd5960074e13b5cc46d0c7327f1b674a3b65b9
Nam Wook Kim <namwkim85@gmail.com>
2017-09-18 09:33:17 -0400

    update many

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-window.onload = function() {
-	var see = document.getElementById('see-old-news');
-
-  see.addEventListener('click', function() {
-    var elem = document.getElementById('news-archive');
-
-  	if (elem.style.display === 'none') {
-  		elem.style.display = 'block';
-      see.innerHTML = 'Hide old news';
-  	} else {
-  		elem.style.display = 'none';
-      see.innerHTML = 'See old news';
-  	}
-	});
-
-}
+// window.onload = function() {
+// 	var see = document.getElementById('see-old-news');
+//
+//   see.addEventListener('click', function() {
+//     var elem = document.getElementById('news-archive');
+//
+//   	if (elem.style.display === 'none') {
+//   		elem.style.display = 'block';
+//       see.innerHTML = 'Hide old news';
+//   	} else {
+//   		elem.style.display = 'none';
+//       see.innerHTML = 'See old news';
+//   	}
+// 	});
+//
+// }
